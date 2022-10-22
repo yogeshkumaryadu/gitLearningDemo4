15259a498d822973e59142837c9db0edd266ea47
yogeshkumaryadu <yogeshkumaryadu@gmail.com>
2022-10-22 08:58:52 +0530

    creating new branch

---
+++ src/main/java/com/example/gitLearningDemo2/GitLearningDemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.gitLearningDemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLearningDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitLearningDemo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitLearningDemo2/GitLearningDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitLearningDemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLearningDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
